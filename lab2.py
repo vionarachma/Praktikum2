64334677aea6c5a2f2578ffcb1e726dfa2c9afb5
vionarachma <92927492+vionarachma@users.noreply.github.com>
2021-11-05 22:14:41 +0700

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,11 @@
+a=int(input("masukan nilai a:"))
+b=int(input("masukan nilai b:"))
+print("variabel a=",a)
+print("variabel b=",b)
+print("hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b))
+
+#konversi nilai variabel
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%f".format(a,b) %(a/b))
